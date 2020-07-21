2fb1869a4ab09de6b03e2e5d3b03f50187e519a4
Matteo Bilotta <me@byloth.net>
2020-07-21 16:35:25 +0200

    [ADD] Added first auto-update for each animation.

--- animations/index.ts
+++ animations/index.ts
@@ -46,6 +46,8 @@ export default class ScrollAnimation
 
         this._init(options);
         this._compile(options);
+
+        this.update();
     }
 
     protected _init(options: AnimationOptions): void
