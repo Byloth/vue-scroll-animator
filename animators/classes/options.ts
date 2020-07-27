f7c3d4b2351e3b31db9ccb644286ef56167d5471
Matteo Bilotta <me@byloth.net>
2020-07-27 13:54:35 +0200

    [LINT] Linted project.

--- animators/classes/options.ts
+++ animators/classes/options.ts
@@ -14,6 +14,6 @@ export default interface ClassAnimatorOptions extends BaseAnimatorOptions
 {
     classesName: string[];
     behavior?: ClassAnimatorBehavior;
-}
+};
 
 export { ClassAnimatorBehavior };
